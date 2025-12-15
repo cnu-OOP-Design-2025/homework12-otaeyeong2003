ef55a53965a1cb97be11c15c330228dd275422ce
202202608 오태영 <otaeyeong2003@gmail.com>
2025-12-15 21:27:41 +0900

    과제제출

--- main.cpp
+++ main.cpp
@@ -1,36 +1,58 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <stdexcept>
 #include "character.h"
 
 using namespace std;
 
 void equip_test1(shared_ptr<Character> c1) {
-    c1 = make_shared<Armor>(c1);
-    c1 = make_shared<Boots>(c1);
-    c1 = make_shared<Sword>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+    try {
+        c1 = make_shared<Armor>(c1);
+        c1 = make_shared<Boots>(c1);
+        c1 = make_shared<Sword>(c1);
+
+        cout << c1->getDescription()
+             << " | Attack: " << c1->getAttack()
+             << " | Speed: " << c1->getSpeed()
+             << " | Defense: " << c1->getDefense() << endl;
+    }
+    catch (const invalid_argument& e) {
+        cout << "Invalid equip: " << e.what() << endl;
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
 void equip_test2(shared_ptr<Character> c1) {
-    c1 = make_shared<Armor>(c1);
-    c1 = make_shared<Bow>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+    try {
+        c1 = make_shared<Armor>(c1);
+        c1 = make_shared<Bow>(c1);
+
+        cout << c1->getDescription()
+             << " | Attack: " << c1->getAttack()
+             << " | Speed: " << c1->getSpeed()
+             << " | Defense: " << c1->getDefense() << endl;
+    }
+    catch (const invalid_argument& e) {
+        cout << "Invalid equip: " << e.what() << endl;
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
 void equip_test3(shared_ptr<Character> c1) {
-    c1 = make_shared<Boots>(c1);
-    c1 = make_shared<Staff>(c1);
-    cout << c1->getDescription()
-        << " | Attack: " << c1->getAttack()
-        << " | Speed: " << c1->getSpeed()
-        << " | Defense: " << c1->getDefense() << endl;
+    try {
+        c1 = make_shared<Boots>(c1);
+        c1 = make_shared<Staff>(c1);
+
+        cout << c1->getDescription()
+             << " | Attack: " << c1->getAttack()
+             << " | Speed: " << c1->getSpeed()
+             << " | Defense: " << c1->getDefense() << endl;
+    }
+    catch (const invalid_argument& e) {
+        cout << "Invalid equip: " << e.what() << endl;
+        Logger::getInstance()->log("[Fail to Equip] ");
+    }
 }
 
 void doTest() {
@@ -39,26 +61,16 @@ void doTest() {
     players.push_back(make_shared<Wizard>());
     players.push_back(make_shared<Archer>());
 
-    for(auto player: players){
+    for (auto player : players) {
         cout << "-------------------------------------------------------------" << endl;
         equip_test1(player);
         equip_test2(player);
         equip_test3(player);
     }
     cout << "-------------------------------------------------------------" << endl;
-
 }
 
-struct Student{
-    string name;
-    Student(string name): name(name) {};
-    Student() = delete;
-    ~Student() { cout << "Delete Student " << name << endl; }
-};
-
-
-
-int main(int argc, const char *argv[]) {
+int main() {
     doTest();
     return 0;
 }
